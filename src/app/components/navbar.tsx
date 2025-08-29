d4d1cb0ed41530b8be12a7fe20b87f5535924ed9
Keith986 <105490335+keith986@users.noreply.github.com>
2025-08-29 10:20:39 +0300

    level9

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -31,19 +31,10 @@ export default function Navbar() {
       <li className="border border-gray-300"></li>
       <li className="hover:scale-95 transition-all">
         <a href="#" className="text-sm flex block md:p-0 text-white-900 rounded-sm md:hover:bg-transparent md:hover:text-white-700 md:dark:hover:text-white-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-          <svg className="w-5 h-5 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <svg className="w-5 h-5 text-white dark:text-white hover:text-red-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 15v3c0 .5523.44772 1 1 1h10.5M3 15v-4m0 4h11M3 11V6c0-.55228.44772-1 1-1h16c.5523 0 1 .44772 1 1v5M3 11h18m0 0v1M8 11v8m4-8v8m4-8v2m1.8956 5.9528 1.5047-1.5047m0 0 1.5048-1.5048m-1.5048 1.5048 1.4605 1.4604m-1.4605-1.4604-1.4604-1.4605"/>
           </svg>
-         <span className="mx-2 text-white">Delete All table</span>
-        </a>
-      </li>
-       <li className="border border-gray-300"></li>
-      <li className="hover:scale-95 transition-all">
-        <a href="#" className="text-sm flex block md:p-0 text-white-900 rounded-sm md:hover:bg-transparent md:hover:text-white-700 md:dark:hover:text-white-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-          <svg className="w-5 h-5 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-          <path stroke="currentColor" strokeLinejoin="round" strokeWidth="2" d="M16.444 18H19a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h2.556M17 11V5a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v6h10ZM7 15h10v4a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1v-4Z"/>
-          </svg>
-         <span className="mx-2 text-white">Print</span>
+         <span className="mx-2 text-white hover:text-red-600">Delete All table</span>
         </a>
       </li>
     </ul>
